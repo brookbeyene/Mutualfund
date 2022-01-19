59c1137f90a38c53f03f3798754e894876578810
brookbeyene <brookbeyene37@gmail.com>
2022-01-19 10:19:51 -0700

    changed stock ticker routes

--- routes/stock/stockTickersRouter.js
+++ routes/stock/stockTickersRouter.js
@@ -4,26 +4,12 @@ const stockTickersController = require('../../controllers/stock/stockTickersCont
 // create a Router object from express
 const tickerRouters = require('express').Router();
 
-//add a new stock api to the table
-tickerRouters.post('/', stockTickersController.addStockTickers)
-//add a new stock api to the table
-tickerRouters.get('/', stockTickersController.importAllTickers)
-// router.get('/import/:userid/:', stockApiController.getOneimportApi)
-// router.post('/import/:userid/:theday/:thesymbol', stockApiController.addImportApi)
 
-// // access all the stock api in the talbe
+tickerRouters.post('/add', stockTickersController.addStockTickers)
+tickerRouters.get('/import', stockTickersController.importAllTickers)
 tickerRouters.get('/all', stockTickersController.getAllStockTickerPost)
 tickerRouters.delete('/delete/all', stockTickersController.deleteAllStockPost)
 
-// // access one stock api by id
-// router.get('/:id', stockApiController.getOneStockApi)
-
-// // modify one stock api by id
-// router.put('/:id', stockApiController.updateStockApi)
-
-// // delete one stock apit by id
-// router.delete('/:id', stockApiController.deleteStockApi)
-// router.delete('/:id', stockApiController.deleteStockApi)
 
 module.exports = tickerRouters
 
